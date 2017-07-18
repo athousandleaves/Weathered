0804299f167e01c2bed91402a5f9b061d2fd7ca0
Nick Keeling <nicholaskeeling@irtnet.austin.cc.tx.us>
2017-07-18 10:40:55 -0500

    add user stories

---
+++ app.js
@@ -0,0 +1,6 @@
+ /* 
+User stories:
+1. I can see the weather in my current location.
+2. I can see a different icon or background image (e.g. snowy mountain, hot desert) depending on the weather.
+3. I can push a button to toggle between Fahrenheit and Celsius.
+ */
