89a25f0f2e8108e525117dff76199964d88bb8a7
athousandleaves <superamare@gmail.com>
2017-07-18 10:56:45 -0500

    add loading animation

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ User stories:
 2. I can see a different icon or background image (e.g. snowy mountain, hot desert) depending on the weather.
 3. I can push a button to toggle between Fahrenheit and Celsius.
  */
+
+var body = document.querySelector(".focus");
+
+//loading icon
+var iDiv = document.createElement("div");
+iDiv.className = "i";
+body.appendChild(iDiv);
+var i = document.createElement("i");
+i.className = "fa fa-refresh fa-spin fa-3x fa-fw";
+i.id = "loader";
+iDiv.appendChild(i);
